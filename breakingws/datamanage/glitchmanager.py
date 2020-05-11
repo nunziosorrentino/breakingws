27cc301ed7aa82fcbf94ffdf0f81d13e1fe9b541
nunzios3 <nstino19@gmail.com>
2020-05-11 14:46:14 +0200

    Multiprocessign works, but too long!

--- breakingws/datamanage/glitchmanager.py
+++ breakingws/datamanage/glitchmanager.py
@@ -13,10 +13,37 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+import os
+import glob
+import logging
+import multiprocessing as mp
 import numpy as np
-import multiprocess
+
+from functools import partial
+from matplotlib.pyplot import imread
 
 from breakingws.datamanage.imagesmanager import ImagesManager
+
+N_CPUS = mp.cpu_count() - 1
+
+def create_images_dict(p_list, duration=2.):
+    print('Process running')
+    imgs_dict = {}    
+    for p_ in p_list:
+        ipaths = os.path.join(p_, 
+                             "*spectrogram_{:.1f}.png".format(duration))
+        path_list = glob.glob(ipaths)
+        key_label = p_.split('/')[-1]
+        new_images = [imread(i) for i in path_list]
+        try:
+            imgs_dict[key_label] = np.append(
+                                           imgs_dict[key_label], new_images
+                                           )
+        except KeyError:
+            imgs_dict[key_label] = new_images 
+        print(p_list, "images imported!!!")
+    print('Done process!')  
+    return imgs_dict
 
 class GlitchManager(ImagesManager):
     """
@@ -25,35 +52,59 @@ class GlitchManager(ImagesManager):
     glitch soectrograms were hand-labeled by citizen scientists.
  
     """
-    #nproc=None
-    def __init__(self, images, interfs, images_id='', duration=2.)
+    def __init__(self, images, images_id='', duration=2.):
         """
         """
         ImagesManager.__init__(self, images, images_id)
-        self.interfs = interfs
         self.duration = duration
         
     @classmethod    
-    def from_directory(cls, dir_path, interfs, duration=2., nproc=None):
+    def from_directory(cls, dir_path, duration=2., nproc=1):
         """
-        This methos enables to collect a 'GlitchManager' instance by simply
+        This methods enables to collect a 'GlitchManager' instance,
         indicating the path to the directory with images divided in 
         interferometers and labels as subdirectories.
         """
         partents_path = os.path.join(dir_path, "*") 
-        p_path_list = glob.glob(partents_path)
-        def create_images_dict(path_list):
-            imgs_dict = {} 
-            for p_ in path_list:
-                ipaths = os.path.join(p_, 
-                         "*spectrogram_{:.1f}.png".format(duration))
-                path_list = glob.glob(ipaths)
-                key_label = p_.split('/')[-1]
-                imgs_dict[key_label] = [imread(i) for i in path_list]
-            return imgs_dict
-        proc = multiprocessing.Pool(processes=n_proc)
-        proc_outputs = proc.map(create_images_dict, p_path_list)
+        interfs_path_list = glob.glob(partents_path)
+        full_path_list = np.array([])
+        for i_ in interfs_path_list: 
+            p_path = os.path.join(i_, "*") 
+            p_path_list = glob.glob(p_path) 
+            full_path_list = np.concatenate((full_path_list, p_path_list))
+        
+        full_path_list = \
+                [x for x in full_path_list if not x.endswith('.csv')]     
+
+        n_files = len(full_path_list)
+        splits_list = [n_files//nproc*(i+1) for i in range(nproc-1)]
+        proc_iterable = np.split(full_path_list, splits_list)
+        
+        # Set logger
+        mp.log_to_stderr()
+        logger = mp.get_logger()
+        logger.setLevel(logging.INFO)
+        
+        proc = mp.Pool(processes=nproc)
+        c_images_dict=partial(create_images_dict, duration=duration)
+        proc_outputs = proc.map(c_images_dict, proc_iterable)
         proc.close()
         proc.join()
-        return cls(proc_outputs, interfs, duration=duration)
+        
+        dict_images = {}
+        for proc in proc_outputs:
+            dict_images.update(proc)
+
+        return cls(dict_images, duration=duration)
+        
+if __name__=='__main__':
+    print('This should be test!!!')
+    #test_images = GlitchManager.from_directory('GravitySpyTrainingSetV1D1',
+    #                                            nproc=N_CPUS)
+    #print('AAAAAAAAAAAAA', test_images.images)
+    #print('BBBBBBBB', test_images.images_ids)
+    #print('CCCCCCCC', test_images.labels)
+    #print('DDDDDDDDDDD', test_images.dict_imgs)
+    #print('EEEEEEEEE', test_images.dict_labs)        
+
         
