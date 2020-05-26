f73a9e4db0a1c975e771512625d6fa0907bb129b
nunzios3 <nstino19@gmail.com>
2020-05-26 17:54:39 +0200

    Genereted images for tests.

--- breakingws/__init__.py
+++ breakingws/__init__.py
@@ -1,1 +1,10 @@
+import os
 from breakingws.version import __version__
+
+# Basic folder structure of the package
+
+BREAKINGWS_ROOT = os.path.abspath(os.path.dirname(__file__))
+BREAKINGWS_BASE = os.path.abspath(os.path.join(BREAKINGWS_ROOT, os.pardir))
+BREAKINGWS_DAMA = os.path.abspath(os.path.join(BREAKINGWS_BASE, 
+                                               'datamanage'))
+BREAKINGWS_DATA = os.path.abspath(os.path.join(BREAKINGWS_DAMA, 'data'))
